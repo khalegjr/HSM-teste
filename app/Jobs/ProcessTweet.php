e02aaa7899e2a12a0bba4598cfd7e9166a76d991
Khaled <khalegjr@gmail.com>
2018-06-25 01:24:09 -0300

    Habilita o processo handle Popula cada tweet Registra TwitterStream no Service Provider

--- app/Jobs/ProcessTweet.php
+++ app/Jobs/ProcessTweet.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Job responsável por retirar o tweets da fila e montar
+ * o tweet
+ */
 
 namespace App\Jobs;
 
@@ -7,21 +11,25 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use App\Tweet;
+use App\TwitterOrthographic;
 
 class ProcessTweet implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
-
+    
+    protected $tweet;
+    
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($tweet)
     {
-        //
+        $this->tweet = $tweet;
     }
-
+    
     /**
      * Execute the job.
      *
@@ -29,6 +37,22 @@ class ProcessTweet implements ShouldQueue
      */
     public function handle()
     {
-        //
+        $tweet = json_decode($this->tweet,true);
+        $tweet_text = isset($tweet['text']) ? $tweet['text'] : null;
+        $user_id = isset($tweet['user']['id_str']) ? $tweet['user']['id_str'] : null;
+        $user_screen_name = isset($tweet['user']['screen_name']) ? $tweet['user']['screen_name'] : null;
+        $user_avatar_url = isset($tweet['user']['profile_image_url_https']) ? $tweet['user']['profile_image_url_https'] : null;
+        
+        if (isset($tweet['id'])) {
+            Tweet::create([
+                'id' => $tweet['id_str'],
+                'json' => $this->tweet,
+                'tweet_text' => $tweet_text,
+                'user_id' => $user_id,
+                'user_screen_name' => $user_screen_name,
+                'user_avatar_url' => $user_avatar_url,
+                'approved' => 0
+            ]);
+        }
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Phirehose;
 use App\TwitterStream;
-use Spatie\LaravelTwitterStreamingApi\TwitterStreamingApi;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,7 +17,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
-
+    
     /**
      * Register any application services.
      *
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind('App\TwitterStream', function ($app) {
+        $this->app->bind('App\TwitterStream', function ($app){
             $twitter_access_token = env('TWITTER_ACCESS_TOKEN', null);
             $twitter_access_token_secret = env('TWITTER_ACCESS_TOKEN_SECRET', null);
             return new TwitterStream($twitter_access_token, $twitter_access_token_secret, Phirehose::METHOD_FILTER);
