dc8c940b1db197bf91f569a20467c36c1c7510c8
Khaled <khalegjr@gmail.com>
2018-06-25 01:40:13 -0300

    Documenta a classe TwitterStream

--- app/TwitterStream.php
+++ app/TwitterStream.php
@@ -8,13 +8,16 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 
 class TwitterStream extends OauthPhirehose
 {
+    /* usa a classe DispatchesJobs para facilitar o envio
+    do tweet para a fila. */
     use DispatchesJobs;
-
+    
     /**
-    * Enqueue each status
-    *
-    * @param string $status
-    */
+     * Enqueue each status
+     *
+     * @param string $status
+     * Chama o Job para processar o tweet
+     */
     public function enqueueStatus($status)
     {
         $this->dispatch(new ProcessTweet($status));
