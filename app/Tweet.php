6a30582adeb3c3f614913f9703fbda72d0cb2ac7
Khaled <khalegjr@gmail.com>
2018-06-25 01:33:37 -0300

    Cria o model para gravar Tweet

---
+++ app/Tweet.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Model para gravar tweet no banco de dados
+ * */
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tweet extends Model
+{
+    protected $fillable = [
+        'id',
+        'json',
+        'tweet_text',
+        'user_id',
+        'user_screen_name',
+        'user_avatar_url',
+        'public',
+        'approved'
+    ];
+}
